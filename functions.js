5fd3b0bbf1d11d75fba58f5c8186f652b0e79dc8
Aleks Cela <alekscela7@gmail.com>
2022-12-02 15:57:43 +0100

    donev2

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 import fsp from 'fs/promises';
 
-async function createAppendFile(filename, text) {
+async function createFile(filename, text) {
     try {
-        await fsp.appendFile(`${filename}`, `${text}\n`)
+        await fsp.writeFile(`${filename}`, `${text}\n`)
     } catch (error) {
         console.log(error.message);
     }
 }
 
-export default createAppendFile;
+export default createFile;
--- index.js
+++ index.js
@@ -1,48 +1,84 @@
 import fsp from 'fs/promises'
-import fs from 'fs'
 import express from 'express';
-import createAppendFile from './functions.js';
 const app = express();
 app.use(express.json());
 
-app.post('/set', async (request, response) => {
-    const body = request.body
-    const bodyv2 = JSON.stringify(body)
-    createAppendFile(`data.json`, `${bodyv2}`)
-    const result = { body };
-    response.status(200);
-    response.json(result);
+app.post('/trip', async (request, response) => {
+    const trip = request.body
+    const database = await fsp.readFile("database.json", "utf-8")
+    const databaseParsed = JSON.parse(database)
+    const id = databaseParsed.length + 1
+    trip.id = id
+    databaseParsed.push(trip)
+    await fsp.writeFile("database.json", JSON.stringify(databaseParsed, null, 4))
+    response.status(200)
+    response.json(trip)
 });
 
-
-app.get('/get', async (request, response) => {
-    try {
-        const theContent = await fsp.readFile('data.json', 'utf-8')
-        console.log(theContent);
-        response.status(200);
-        response.json(theContent);
-    } catch (error) {
-        console.log(error.message);
-        response.send(false)
-    }
-});
-
-
-app.delete('/remove', async (request, response) => {
-    try {
-        await fsp.unlink('data.json')
-        response.status(200);
-        response.json(true);
-    } catch (error) {
-        console.log(error.message);
-        response.send(false)
+app.get('/trip/:id', async (request, response) => {
+    const id = Number(request.params.id)
+    const database = await fsp.readFile("database.json", "utf-8")
+    const databaseParsed = JSON.parse(database)
+    const trip = databaseParsed.find((trip) => {
+        return trip.id === id
+    })
+    if (trip) {
+        response.status(200)
+        response.json(trip)
+    } else {
+        response.status(404)
+        response.json(`Trip with the selected id: ${id} , does not exist`)
     }
 
 });
+
+app.get('/trip', async (request, response) => {
+    const database = await fsp.readFile("database.json", "utf-8")
+    const databaseParsed = JSON.parse(database)
+    response.status(200)
+    response.json(databaseParsed)
+});
+
+app.put('/trip/:id/:destination', async (request, response) => {
+    const id = Number(request.params.id)
+    const newDestination = request.params.destination
+    const database = await fsp.readFile("database.json", "utf-8")
+    const databaseParsed = JSON.parse(database)
+    const trip = databaseParsed.find((trip) => {
+        return trip.id === id
+    })
+    trip.destination = newDestination
+    console.log(trip);
+    const index = databaseParsed.indexOf(trip);
+    if (index > -1) {
+        databaseParsed.splice(index, 1);
+    }
+    databaseParsed.push(trip)
+    await fsp.writeFile("database.json", JSON.stringify(databaseParsed, null, 4))
+    response.status(200)
+    response.json(trip)
+});
+
+app.delete('/trip/:id', async (request, response) => {
+    const id = Number(request.params.id)
+    const database = await fsp.readFile("database.json", "utf-8")
+    const databaseParsed = JSON.parse(database)
+    const trip = databaseParsed.find((trip) => {
+        return trip.id === id
+    })
+    const index = databaseParsed.indexOf(trip);
+    if (index > -1) {
+        databaseParsed.splice(index, 1);
+    }
+    await fsp.writeFile("database.json", JSON.stringify(databaseParsed, null, 4))
+    response.status(200)
+    response.json(true)
+});
+
 
 app.all('/*', async (request, response) => {
     response.status(404);
-    response.json({ error: 'I can only give you authors, this route does not exist' });
+    response.json({ error: 'This route does not exist' });
 });
 
 const hostname = 'localhost';
